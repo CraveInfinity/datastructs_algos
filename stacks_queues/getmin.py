d211d34731c27815f3dbac0711223b4c594690fa
66929803 <yashwanth.remidi@target.com>
2017-11-05 19:59:10 +0530

    changed getmin.py :: return pass

--- stacks_queues/getmin.py
+++ stacks_queues/getmin.py
@@ -1,30 +1,31 @@
-# class MinStack:
-#     def __init__(self):
-#         self.items = []
-#         self.m = []
-#
-#     # @param x, an integer
-#     def push(self, x):
-#         self.items.append(x)
-#
-#
-#
-#
-#     # @return nothing
-#     def pop(self):
-#         if self.items:
-#             self.items.pop()
-#
-#     # @return an integer
-#     def top(self):
-#         if self.items:
-#             return self.items[-1]
-#         else:
-#             return -1
-#
-#     # @return an integer
-#     def getMin(self):
-#
-#
-#
-#
+class MinStack:
+    def __init__(self):
+        self.items = []
+        self.m = []
+
+    # @param x, an integer
+    def push(self, x):
+        self.items.append(x)
+
+
+
+
+    # @return nothing
+    def pop(self):
+        if self.items:
+            self.items.pop()
+
+    # @return an integer
+    def top(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            return -1
+
+    # @return an integer
+    def getMin(self):
+        pass
+
+
+
+
