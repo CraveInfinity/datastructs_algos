be768e5ed78c111f2e6289ed836f8d18444cccab
66929803 <yashwanth.remidi@target.com>
2017-11-07 21:50:15 +0530

    inefficient logic ----- check the solution

--- backtracking/kth permutation.py
+++ backtracking/kth permutation.py
@@ -15,6 +15,8 @@ class Solution:
 
     def getPermutation(self, n, k):
 
+        self.count = 0
+
         m = self.do_factorial_search(k)
         lm = n - m
 
@@ -26,27 +28,24 @@ class Solution:
         self.k = k
         A = list(xrange(1, n + 1))[lm:]
         self.A = A
-        self.final = []
 
         self.xpermute([], A)
-        z = ''.join([str(e) for e in self.final[-1]])
+        z = ''.join([str(e) for e in self.hero])
         return init + z
 
     def xpermute(self, res, m):
 
         if len(res) == len(self.A):
-            self.final.append(res)
+            self.count += 1
+            if self.count == self.k:
+                self.hero = res
             return
 
         i = 0
         while i < len(m):
-            if len(self.final) == self.k:
+            if self.count == self.k:
                 break
-                return
             tres = res + [m[i]]
             l = [m[k] for k in xrange(len(m)) if not k == i]
             self.xpermute(tres, l)
             i += 1
-
-
-print Solution().getPermutation(8, 81)
