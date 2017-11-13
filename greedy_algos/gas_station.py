d15a5e8290133ded90bd44da0d07ba342551abb1
66929803 <yashwanth.remidi@target.com>
2017-11-13 17:22:37 +0530

    initial commit -- still O(n*2) solution inefficient - but added condition when no return -1 in O(n) and it satisfies

--- greedy_algos/gas_station.py
+++ greedy_algos/gas_station.py
@@ -1,3 +1,46 @@
+# class Solution:
+#     # @param gas : tuple of integers
+#     # @param cost : tuple of integers
+#     # @return an integer
+#     def canCompleteCircuit(self, gas, cost):
+#
+#         if len(gas) <= 1:
+#             return 0
+#
+#         l = len(gas)
+#
+#         g = 0
+#         while g < len(gas):
+#
+#             init = g
+#             nxt = g
+#             first = True
+#             car = 0
+#             broke = False
+#             while not nxt == init or first:
+#                 if first:
+#                     car = gas[init]
+#                     car -= cost[init]
+#                     first = False
+#                 else:
+#                     car += gas[nxt]
+#                     car -= cost[nxt]
+#
+#                 if car >= 0:
+#                     nxt += 1
+#                     nxt = nxt % l
+#                 else:
+#                     broke = True
+#                     break
+#
+#             if not broke:
+#                 return init
+#
+#             g += 1
+#
+#         return -1
+
+
 class Solution:
     # @param gas : tuple of integers
     # @param cost : tuple of integers
@@ -7,8 +50,12 @@ class Solution:
         if len(gas) <= 1:
             return 0
 
-        l = len(gas)
+        diffs = [g-c for g, c in zip(gas, cost)]
 
+        if sum(diffs) < 0:
+            return -1
+
+        l = len(gas)
         g = 0
         while g < len(gas):
 
@@ -38,7 +85,6 @@ class Solution:
 
             g += 1
 
-        return -1
 
 A = [1, 2]
 B = [2, 1]
