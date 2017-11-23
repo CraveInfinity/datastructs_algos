f0e6590b9aeb994a0ca177e4a7dad42c9c5c5694
66929803 <yashwanth.remidi@target.com>
2017-11-23 11:52:47 +0530

    initial commit -- check solution

---
+++ graphs/longest_tree_path.py
@@ -0,0 +1,18 @@
+"""
+    longest path between two points :::
+                    - for any node 'u', if node 'x' is the farthest node then 'x' is one of the endpoints
+                        in the longest path.
+
+                    So solution,
+                        For any node 'u', find its farthest node 'x' and then
+                        find the farthest node to 'x', node 't',
+
+                        d(x, t) is answer
+
+"""
+
+
+
+
+
+
